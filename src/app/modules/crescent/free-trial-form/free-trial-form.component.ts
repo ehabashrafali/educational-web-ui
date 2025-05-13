7cf38e78169c79cd50c139bc6ae4ae4338d29628
ehabashrafali <ehabashrafali88@gmail.com>
2025-05-13 22:47:38 +0300

    adding free trial comp

--- src/app/modules/crescent/free-trial-form/free-trial-form.component.ts
+++ src/app/modules/crescent/free-trial-form/free-trial-form.component.ts
@@ -1,5 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatOptionModule } from '@angular/material/core';
@@ -10,12 +11,14 @@ import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { CountrySelectComponent } from '@wlucha/ng-country-select';
-
+import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import { SearchCountryField, CountryISO, PhoneNumberFormat } from 'ngx-intl-tel-input';
 
 @Component({
   selector: 'app-free-trial-form',
   standalone: true,
-  imports: [MatIconModule,
+  imports: [
+    MatIconModule,
     FormsModule,
     MatFormFieldModule,
     MatInputModule,
@@ -25,6 +28,8 @@ import { CountrySelectComponent } from '@wlucha/ng-country-select';
     MatCheckboxModule,
     MatRadioModule,
     MatButtonModule,
+    NgxIntlTelInputModule,
+    ReactiveFormsModule,
     CountrySelectComponent],
   templateUrl: './free-trial-form.component.html',
   styleUrl: './free-trial-form.component.scss'
@@ -33,7 +38,25 @@ export class FreeTrialFormComponent {
   preferredChannel: any;
   preferredTeacher: any;
   learnAbout: any;
-  phoneControl = new FormControl('');
+  SearchCountryField = SearchCountryField;
+  preferredCountries: CountryISO[] = [CountryISO.UnitedStates, CountryISO.UnitedKingdom];
+  PhoneNumberFormat = PhoneNumberFormat;
+
+
+
+  form = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', Validators.required),
+    studentCount: new FormControl(1),
+    preferredTeacher: new FormControl('either'),
+    preferredChannel: new FormControl('email'),
+    learnAbout: new FormControl('social'),
+    interestedIn: new FormControl('1to1'),
+    notes: new FormControl('')
+  });
+
 
   handleSelection($event: any) {
     throw new Error('Method not implemented.');
