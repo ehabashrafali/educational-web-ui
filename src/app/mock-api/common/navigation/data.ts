b6481bb19e6710ae8d592491c4ed7768aaff62fe
Ehab Ashraf Ali <eaali@efghldg.com>
2025-06-28 17:13:07 +0300

    adding review intial

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -1,82 +1,75 @@
 /* eslint-disable */
-import { FuseNavigationItem } from '@fuse/components/navigation';
+import { FuseNavigationItem } from "@fuse/components/navigation";
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'Example',
-        type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
-    }
+        id: "example",
+        title: "Example",
+        type: "basic",
+        icon: "heroicons_outline:chart-pie",
+        link: "/example",
+    },
 ];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'sdsds',
-        type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
-    }
+        id: "example",
+        title: "sdsds",
+        type: "basic",
+        icon: "heroicons_outline:chart-pie",
+        link: "/example",
+    },
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'Example',
-        type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
-    }
+        id: "example",
+        title: "Example",
+        type: "basic",
+        icon: "heroicons_outline:chart-pie",
+        link: "/example",
+    },
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
     {
-        id: 'explore',
-        title: 'Explore',
-        type: 'collapsable',
-        icon: 'heroicons_outline:building-library',
+        id: "explore",
+        title: "Explore",
+        type: "collapsable",
+        icon: "heroicons_outline:building-library",
         children: [
             {
-                id: 'home',
-                title: 'Home',
-                type: 'basic',
-                icon: 'heroicons_outline:home',
-                link: '/home'
+                id: "home",
+                title: "Home",
+                type: "basic",
+                icon: "heroicons_outline:home",
+                link: "/home",
             },
             {
-                id: 'about',
-                title: 'About',
-                type: 'basic',
-                icon: 'heroicons_outline:information-circle',
-                link: '/about'
+                id: "faqs",
+                title: "FAQs",
+                type: "basic",
+                icon: "heroicons_outline:question-mark-circle",
+                link: "/faqs",
             },
             {
-                id: 'faqs',
-                title: 'FAQs',
-                type: 'basic',
-                icon: 'heroicons_outline:question-mark-circle',
-                link: '/faqs',
-            },
-            {
-                id: 'support',
-                title: 'Contact Us',
-                type: 'basic',
-                icon: 'heroicons_outline:lifebuoy',
-                link: '/support',
+                id: "support",
+                title: "Contact Us",
+                type: "basic",
+                icon: "heroicons_outline:lifebuoy",
+                link: "/support",
             },
         ],
     },
     {
-        id: 'courses',
-        title: 'Courses',
-        type: 'basic',
-        icon: 'heroicons_outline:book-open',
-        link: '/courses'
+        id: "courses",
+        title: "Courses",
+        type: "basic",
+        icon: "heroicons_outline:book-open",
+        link: "/courses",
     },
     {
-        id: 'pricing',
-        title: 'Subscription',
-        type: 'basic',
-        icon: 'heroicons_outline:credit-card',
-        link: '/pricing'
+        id: "pricing",
+        title: "Subscription",
+        type: "basic",
+        icon: "heroicons_outline:credit-card",
+        link: "/pricing",
     },
 ];
