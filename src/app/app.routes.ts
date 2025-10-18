4c6906224e581c3a2b9fc1693dd67805f93a061d
Ehab Ashraf Ali <eaali@efghldg.com>
2025-10-19 00:33:40 +0300

    custom arabic course price

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,199 +8,189 @@ import { LayoutComponent } from "app/layout/layout.component";
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 export const appRoutes: Route[] = [
-    // Redirect empty path to '/example'
-    { path: "", pathMatch: "full", redirectTo: "home" },
+  // Redirect empty path to '/example'
+  { path: "", pathMatch: "full", redirectTo: "home" },
 
-    // Redirect signed-in user to the '/example'
-    //
-    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
-    // path. Below is another redirection for that path to redirect the user to the desired
-    // location. This is a small convenience to keep all main routes together here on this file.
-    { path: "signed-in-redirect", pathMatch: "full", redirectTo: "example" },
+  // Redirect signed-in user to the '/example'
+  //
+  // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+  // path. Below is another redirection for that path to redirect the user to the desired
+  // location. This is a small convenience to keep all main routes together here on this file.
+  { path: "signed-in-redirect", pathMatch: "full", redirectTo: "example" },
 
-    // Auth routes for guests
-    {
-        path: "",
-        canActivate: [NoAuthGuard],
-        canActivateChild: [NoAuthGuard],
-        component: LayoutComponent,
-        data: {
-            layout: "empty",
-        },
-        children: [
-            {
-                path: "confirmation-required",
-                loadChildren: () =>
-                    import(
-                        "app/modules/auth/confirmation-required/confirmation-required.routes"
-                    ),
-            },
-            {
-                path: "forgot-password",
-                loadChildren: () =>
-                    import(
-                        "app/modules/auth/forgot-password/forgot-password.routes"
-                    ),
-            },
-            {
-                path: "reset-password",
-                loadChildren: () =>
-                    import(
-                        "app/modules/auth/reset-password/reset-password.routes"
-                    ),
-            },
-            {
-                path: "sign-in",
-                loadChildren: () =>
-                    import("app/modules/auth/sign-in/sign-in.routes"),
-            },
-            {
-                path: "sign-up",
-                loadChildren: () =>
-                    import("app/modules/auth/sign-up/sign-up.routes"),
-            },
-        ],
+  // Auth routes for guests
+  {
+    path: "",
+    canActivate: [NoAuthGuard],
+    canActivateChild: [NoAuthGuard],
+    component: LayoutComponent,
+    data: {
+      layout: "empty",
     },
+    children: [
+      {
+        path: "confirmation-required",
+        loadChildren: () =>
+          import(
+            "app/modules/auth/confirmation-required/confirmation-required.routes"
+          ),
+      },
+      {
+        path: "forgot-password",
+        loadChildren: () =>
+          import("app/modules/auth/forgot-password/forgot-password.routes"),
+      },
+      {
+        path: "reset-password",
+        loadChildren: () =>
+          import("app/modules/auth/reset-password/reset-password.routes"),
+      },
+      {
+        path: "sign-in",
+        loadChildren: () => import("app/modules/auth/sign-in/sign-in.routes"),
+      },
+      {
+        path: "sign-up",
+        loadChildren: () => import("app/modules/auth/sign-up/sign-up.routes"),
+      },
+    ],
+  },
 
-    // Auth routes for authenticated users
-    {
-        path: "",
-        canActivate: [AuthGuard],
-        canActivateChild: [AuthGuard],
-        component: LayoutComponent,
-        data: {
-            layout: "empty",
-        },
-        children: [
-            {
-                path: "sign-out",
-                loadChildren: () =>
-                    import("app/modules/auth/sign-out/sign-out.routes"),
-            },
-            {
-                path: "unlock-session",
-                loadChildren: () =>
-                    import(
-                        "app/modules/auth/unlock-session/unlock-session.routes"
-                    ),
-            },
-        ],
+  // Auth routes for authenticated users
+  {
+    path: "",
+    canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: LayoutComponent,
+    data: {
+      layout: "empty",
     },
+    children: [
+      {
+        path: "sign-out",
+        loadChildren: () => import("app/modules/auth/sign-out/sign-out.routes"),
+      },
+      {
+        path: "unlock-session",
+        loadChildren: () =>
+          import("app/modules/auth/unlock-session/unlock-session.routes"),
+      },
+    ],
+  },
 
-    // Landing routes
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "home",
-                loadChildren: () =>
-                    import("app/modules/landing/home/home.routes"),
-            },
-        ],
+  // Landing routes
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "about",
-                loadChildren: () =>
-                    import("app/modules/landing/about/about.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "home",
+        loadChildren: () => import("app/modules/landing/home/home.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "faqs",
-                loadChildren: () =>
-                    import("app/modules/landing/faqs/faqs.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "about",
+        loadChildren: () => import("app/modules/landing/about/about.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "support",
-                loadChildren: () =>
-                    import("app/modules/landing/support/support.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "faqs",
+        loadChildren: () => import("app/modules/landing/faqs/faqs.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    // Admin routes
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "courses",
-                loadChildren: () =>
-                    import("app/modules/crescent/courses/courses.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "support",
+        loadChildren: () =>
+          import("app/modules/landing/support/support.routes"),
+      },
+    ],
+  },
+  // Admin routes
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "blogs",
-                loadChildren: () =>
-                    import("app/modules/crescent/blogs/blogs.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "courses",
+        loadChildren: () =>
+          import("app/modules/crescent/courses/courses.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "free-session",
-                loadChildren: () =>
-                    import(
-                        "app/modules/crescent/free-trial-form/free-trail.routes"
-                    ),
-            },
-        ],
+    children: [
+      {
+        path: "blogs",
+        loadChildren: () => import("app/modules/crescent/blogs/blogs.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
-    {
-        path: "",
-        component: LayoutComponent,
-        resolve: {
-            initialData: initialDataResolver,
-        },
-        children: [
-            {
-                path: "pricing",
-                loadChildren: () =>
-                    import("app/modules/payment/pricing/pricing.routes"),
-            },
-        ],
+    children: [
+      {
+        path: "free-session",
+        loadChildren: () =>
+          import("app/modules/crescent/free-trial-form/free-trail.routes"),
+      },
+    ],
+  },
+  {
+    path: "",
+    component: LayoutComponent,
+    resolve: {
+      initialData: initialDataResolver,
     },
+    children: [
+      {
+        path: "pricing/default",
+        loadChildren: () =>
+          import("app/modules/payment/pricing/pricing.routes"),
+      },
+      {
+        path: "pricing/custom",
+        loadChildren: () =>
+          import("app/modules/payment/pricing/pricing.routes"),
+      },
+    ],
+  },
 ];
--- src/app/modules/crescent/free-trial-form/free-trial-form.component.ts
+++ src/app/modules/crescent/free-trial-form/free-trial-form.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, inject, Injector } from "@angular/core";
 import {
-    FormControl,
-    FormGroup,
-    FormsModule,
-    ReactiveFormsModule,
-    Validators,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
 } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCheckboxModule } from "@angular/material/checkbox";
@@ -24,96 +24,96 @@ import { courses } from "app/mock-api/apps/academy/data";
 import { ToastService } from "app/shared/sevices/toasts.service";
 
 @Component({
-    selector: "app-free-trial-form",
-    standalone: true,
-    imports: [
-        CommonModule,
-        MatIconModule,
-        FormsModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule,
-        MatOptionModule,
-        MatDividerModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatButtonModule,
-        ReactiveFormsModule,
-        RouterLink,
-        CountrySelectComponent,
-        FuseLoadingBarComponent
-    ],
-    templateUrl: "./free-trial-form.component.html",
-    styleUrl: "./free-trial-form.component.scss",
+  selector: "app-free-trial-form",
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatDividerModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    RouterLink,
+    CountrySelectComponent,
+    FuseLoadingBarComponent,
+  ],
+  templateUrl: "./free-trial-form.component.html",
+  styleUrl: "./free-trial-form.component.scss",
 })
 export class FreeTrialFormComponent {
-    
-    preferredChannel: any;
-    preferredTeacher: any;
-    learnAbout: any;
-   
-    private _fuseLoadingService = inject(FuseLoadingService);
-    private _courses = Object.values(Courses);
-   
-    public get courses(): Array<string> {
-        return this._courses;
-    }
+  preferredChannel: any;
+  preferredTeacher: any;
+  learnAbout: any;
 
-    form = new FormGroup({
-        firstName: new FormControl("", Validators.required),
-        lastName: new FormControl("", Validators.required),
-        email: new FormControl("", [Validators.required, Validators.email]),
-        phone: new FormControl("", Validators.required),
-        studentCount: new FormControl(1),
-        courses: new FormControl([]),
-        preferredTeacher: new FormControl("either"),
-        preferredChannel: new FormControl("email"),
-        learnAbout: new FormControl("social"),
-        interestedIn: new FormControl("1to1"),
-        notes: new FormControl(""),
-    });
-    /**
-     *
-     */
+  private _fuseLoadingService = inject(FuseLoadingService);
+  private _courses = Object.values(Courses);
 
-    submitForm(): void {
-        if (this.form.valid) {
-            const values = this.form.value;
-        this._fuseLoadingService.show(); 
-            const payload = {
-                ...values,
-                courses: Array.isArray(values.courses) ? values.courses.join(', ') : values.courses
-            };
-            fetch(
-                "https://script.google.com/macros/s/AKfycbx3uRkU9-x0PRLHpqpOEpI0Sbe346GcDPCC18FGx7ObcVxRnR8VUztu9mEsasZt7cnx/exec",
-                {
-                    method: "POST",
-                    mode: "no-cors",
-                    body: JSON.stringify(payload),
-                }
-            )
-                .then(() => {
-                    this.form.reset();
-                    alert("Form submitted successfully!");
-                })
-                .catch((err) => {
-                    console.error("Submission error:", err);
-                    alert("Something went wrong");
-                })
-                .finally(() => {
-                    this._fuseLoadingService.hide();
-                });
-        } else {
-            console.log("Form is invalid");
+  public get courses(): Array<string> {
+    return this._courses;
+  }
+
+  form = new FormGroup({
+    firstName: new FormControl("", Validators.required),
+    lastName: new FormControl("", Validators.required),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    phone: new FormControl("", Validators.required),
+    studentCount: new FormControl(1),
+    courses: new FormControl([]),
+    preferredTeacher: new FormControl("either"),
+    preferredChannel: new FormControl("email"),
+    learnAbout: new FormControl("social"),
+    notes: new FormControl(""),
+  });
+  /**
+   *
+   */
+
+  submitForm(): void {
+    if (this.form.valid) {
+      const values = this.form.value;
+      this._fuseLoadingService.show();
+      const payload = {
+        ...values,
+        courses: Array.isArray(values.courses)
+          ? values.courses.join(", ")
+          : values.courses,
+      };
+      fetch(
+        "https://script.google.com/macros/s/AKfycbx3uRkU9-x0PRLHpqpOEpI0Sbe346GcDPCC18FGx7ObcVxRnR8VUztu9mEsasZt7cnx/exec",
+        {
+          method: "POST",
+          mode: "no-cors",
+          body: JSON.stringify(payload),
         }
+      )
+        .then(() => {
+          this.form.reset();
+          alert("Form submitted successfully!");
+        })
+        .catch((err) => {
+          console.error("Submission error:", err);
+          alert("Something went wrong");
+        })
+        .finally(() => {
+          this._fuseLoadingService.hide();
+        });
+    } else {
+      console.log("Form is invalid");
     }
+  }
 }
 
 export enum Courses {
-    BasicQuranRecitation = 'Basic Quran Recitation',
-    ArabicLanguage = 'Arabic Language Course',
-    Tajweed = 'Tajweed Course',
-    IslamicStudies = 'Islamic Studies Course',
-    QuranicArabic = 'Quranic Arabic Course',
-    Hafiz = 'Hafiz Course'
+  BasicQuranRecitation = "Basic Quran Recitation Course",
+  Tajweed = "Tajweed Course",
+  IslamicStudies = "Islamic Studies Course",
+  QuranicArabic = "Quranic Arabic Course",
+  Hafiz = "Hafiz Course",
+  ArabicLanguage = "Arabic As A Second Language Course",
 }
