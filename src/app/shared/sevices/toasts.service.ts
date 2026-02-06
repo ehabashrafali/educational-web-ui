66a388693350395b3ca5ed26efe754b24072700a
ehabashrafali <ehabashrafali88@gmail.com>
2026-02-06 17:08:50 +0200

    enhancemets

--- src/app/shared/sevices/toasts.service.ts
+++ src/app/shared/sevices/toasts.service.ts
@@ -4,79 +4,78 @@ import { tap } from "rxjs";
 
 @Injectable({ providedIn: "root" })
 export class ToastService {
-    toasts: ToastMessage[] = [];
+  toasts: ToastMessage[] = [];
 
-    constructor(private notificationService: NotificationService) {}
+  constructor(private notificationService: NotificationService) {}
 
-    error(toastMessage: ToastMessage) {
-        this.add({
-            ...toastMessage,
-            type: "danger",
-            autoHide: true,
-        });
-    }
+  error(toastMessage: ToastMessage) {
+    this.add({
+      ...toastMessage,
+      type: "danger",
+      autoHide: true,
+    });
+  }
 
-    warning(toastMessage: ToastMessage) {
-        this.add({
-            ...toastMessage,
-            type: "warning",
-            autoHide: true,
-        });
-    }
+  warning(toastMessage: ToastMessage) {
+    this.add({
+      ...toastMessage,
+      type: "warning",
+      autoHide: true,
+    });
+  }
 
-    info(toastMessage: ToastMessage) {
-        this.add({
-            ...toastMessage,
-            autoHide: true,
-            type: "info",
-        });
-    }
+  info(toastMessage: ToastMessage) {
+    this.add({
+      ...toastMessage,
+      autoHide: true,
+      type: "info",
+    });
+  }
 
-    success(toastMessage: ToastMessage) {
-        this.add({
-            ...toastMessage,
-            autoHide: true,
-            type: "success",
-        });
-    }
+  success(toastMessage: ToastMessage) {
+    this.add({
+      ...toastMessage,
+      autoHide: true,
+      type: "success",
+    });
+  }
 
-    private add(toastMessage: ToastMessage) {
-        this.toasts.push(toastMessage);
-        this.notificationService.notify(
-            toastMessage.title,
-            !toastMessage.button?.text
-                ? toastMessage.message
-                : toastMessage.message +
-                      ` (click to ${toastMessage.button.text})`,
-            () => {
-                this.remove(toastMessage);
-                if (toastMessage.button?.action) toastMessage.button.action();
-            }
-        );
-    }
+  private add(toastMessage: ToastMessage) {
+    this.toasts.push(toastMessage);
+    this.notificationService.notify(
+      toastMessage.title,
+      !toastMessage.button?.text
+        ? toastMessage.message
+        : toastMessage.message + ` (click to ${toastMessage.button.text})`,
+      () => {
+        this.remove(toastMessage);
+        if (toastMessage.button?.action) toastMessage.button.action();
+      },
+    );
+  }
 
-    remove(toast) {
-        this.toasts = this.toasts.filter((t) => t !== toast);
-    }
+  remove(toast) {
+    this.toasts = this.toasts.filter((t) => t !== toast);
+  }
 }
 
 export const showToastOnSuccess = (
-    ToastService: ToastService,
-    toastMessage: ToastMessage
+  ToastService: ToastService,
+  toastMessage: ToastMessage,
 ) => tap(() => ToastService.success(toastMessage));
 
 export interface ToastMessage {
-    title?: string;
-    message?: string;
-    template?: TemplateRef<any>;
-    button?: {
-        text: string;
-        color?: "primary" | "accent" | "warning" | "danger" | "info";
-        action: () => void;
-    };
-    type?: "danger" | "success" | "info" | "warning";
-    delayTime?: number;
-    autoHide?: boolean;
-    className?: string;
-    time?: Date;
+  title?: string;
+  message?: string;
+  template?: TemplateRef<any>;
+  button?: {
+    text: string;
+    color?: "primary" | "accent" | "warning" | "danger" | "info";
+    action: () => void;
+  };
+  type?: "danger" | "success" | "info" | "warning";
+  delayTime?: number;
+  autoHide?: boolean;
+  className?: string;
+  time?: Date;
 }
