75db7d23e68b6bc108efdb2a019c5ce8729f37d4
ehabashrafali <ehabashrafali88@gmail.com>
2026-02-06 14:31:51 +0200

    toast

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { isDevMode } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "app/app.component";
